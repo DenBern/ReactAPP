4845370699f0c5cebc339f1d7c7306587607ebc3
Denis Bernovich <denisbernovich@gmail.com>
2023-01-11 21:41:29 +0300

    react_123

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,35 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Header } from './App';
 
-const text = 'Hello world';
-
-// Участок кода ниже, является реакт элементом
-const elem = (
-  <div>
-    <h2 className='text'>{text}</h2>
-    <input type="text" />
-    <label htmlFor=''></label>
-    <button tabIndex="0">Click</button>
-    
-  </div>
-);
-
-
-// Этот синтаксис не используется.
-// const elem = React.createElement('h1', {className: 'greeting'}, 'Hello world!');
-// const element = {
-//   type: 'h1',
-//   props: {
-//     className: 'greeting',
-//     children: 'Hello world!'
-//   }
-// }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  elem,
+ReactDOM.render(
+  <App/>,
   document.getElementById('root')
 );
 
